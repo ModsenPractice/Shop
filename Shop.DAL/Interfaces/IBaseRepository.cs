acd28ffd653690579c3003a69d7e509a0a450f37
Kirill Meleshko <meleshkokirill004@gmail.com>
2024-06-21 14:12:55 +0300

    Added minor changes to IBaseRepository (nullable GetSingle return type, Predicate replaced with Expression<Func<T, bool>>

--- Shop.DAL/Interfaces/IBaseRepository.cs
+++ Shop.DAL/Interfaces/IBaseRepository.cs
@@ -1,10 +1,13 @@
+using System.Linq.Expressions;
+
 namespace Shop.DAL.Interfaces
 {
    public interface IBaseRepository<T>
    {
       //Get single entity by condition. Throws exception if more that 1 entity exists
-      public Task<T> GetSingle(Predicate<T> condition, CancellationToken cancellationToken);
-      public Task<IEnumerable<T>> GetRange(Predicate<T> condition,
+      public Task<T?> GetSingle(Expression<Func<T, bool>> condition,
+         CancellationToken cancellationToken);
+      public Task<IEnumerable<T>> GetRange(Expression<Func<T, bool>> condition,
          CancellationToken cancellationToken);
       public void Create(T entity);
       public void Update(T entity);
