e4a3995b5c60d896b5ca8482404fc8fcf5f3b272
des1de <des1de377@gmail.com>
2024-06-21 19:40:00 +0300

    add identity

--- Shop.API/Program.cs
+++ Shop.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Shop.DAL.Contexts;
+using Shop.DAL.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddIdentity<User, IdentityRole>()
+    .AddEntityFrameworkStores<ShopContext>();
 
 var app = builder.Build();
 
