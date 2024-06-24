be3beef185d4c6e2eaed52f2d9cb491233dfcba0
POOLSH <kir180920032003@gmail.com>
2024-06-24 09:06:57 +0300

    Redesigned using

---
+++ Shop.DAL/Repositories/GameRepository.cs
@@ -0,0 +1,15 @@
+﻿using Shop.DAL.Interfaces;
+using Shop.DAL.Models;
+using Shop.DAL.Contexts;
+
+namespace Shop.DAL.Repositories
+{
+    public class GameRepository : BaseRepository<Game>, IGameRepository
+    {
+        public GameRepository(ShopContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+    }
+}
