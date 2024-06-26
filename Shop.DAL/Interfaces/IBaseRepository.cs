1746e6f209a90d26759b45ac24092162c7a8f127
Kirill Meleshko <meleshkokirill004@gmail.com>
2024-06-26 09:24:12 +0300

    Added AsNoTracking to get methods. Replaces SingleOrDefault with FirstOrDefault

--- Shop.DAL/Interfaces/IBaseRepository.cs
+++ Shop.DAL/Interfaces/IBaseRepository.cs
@@ -4,7 +4,6 @@ namespace Shop.DAL.Interfaces
 {
    public interface IBaseRepository<T>
    {
-      //Get single entity by condition. Throws exception if more that 1 entity exists
       public Task<T?> GetSingle(Expression<Func<T, bool>> condition,
          CancellationToken cancellationToken);
       public Task<IEnumerable<T>> GetRange(Expression<Func<T, bool>> condition,
