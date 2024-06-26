617226ebbd8992e4a2ef5b98e92c26215d0795c4
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2024-06-26 09:26:36 +0300

    Create global exception handling

---
+++ Shop.API/ActionFilters/HttpGlobalExceptionFilter.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Shop.API.ActionFilters.Extensions;
+using Shop.BLL.Common.Exceptions.BadRequestExceptions;
+using Shop.BLL.Common.Exceptions.NotFoundExceptions;
+
+namespace Shop.API.ActionFilters;
+
+public class HttpGlobalExceptionFilter(IHostEnvironment environment) : IAsyncExceptionFilter
+{
+    private readonly IHostEnvironment _environment = environment ?? throw new ArgumentNullException(nameof(environment));
+
+    public Task OnExceptionAsync(ExceptionContext context)
+    {
+        context.ExceptionHandled = true;
+        var exception = context.Exception;
+
+        var task = exception switch
+        {
+            ValidationException validationException => HandleValidationException(context, validationException),
+            BadRequestException badRequestException => HandleBadRequestException(context, badRequestException),
+            NotFoundException notFoundException => HandleNotFoundException(context, notFoundException),
+            _ => HandleError(context),
+        };
+
+        return task;
+    }
+
+    private Task HandleValidationException(ExceptionContext context, ValidationException validationException)
+    {
+        return context.WriteErrorAsync(400,
+            _environment.IsDevelopment()
+                ? validationException.Errors.Select(x => x.ErrorMessage).ToArray()
+                : ["Bad request"]);
+    }
+
+    private Task HandleNotFoundException(ExceptionContext context, NotFoundException notFoundException)
+    {
+        return context.WriteErrorAsync(404,
+            _environment.IsDevelopment() ? notFoundException.Message : "Resource not found");
+    }
+
+    private Task HandleBadRequestException(ExceptionContext context, BadRequestException badHttpRequestException)
+    {
+        return context.WriteErrorAsync(400,
+            _environment.IsDevelopment() ? badHttpRequestException.Message : "Bad request");
+    }
+
+    private Task HandleError(ExceptionContext context)
+    {
+        return context.WriteErrorAsync(500,
+            _environment.IsDevelopment() ? context.Exception.Message : "An unexpected error occured");
+    }
+}
--- Shop.API/Program.cs
+++ Shop.API/Program.cs
@@ -1,8 +1,14 @@
+using Shop.API.ActionFilters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add<HttpGlobalExceptionFilter>();
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
