2c4a88271db60fb12603413ff6a294d81aae0151
Егор Толоконников <90565476+C0Rs1K@users.noreply.github.com>
2024-06-28 17:28:09 +0300

    Create order controller

---
+++ Shop.API/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.BLL.Common.DataTransferObjects.Orders;
+using Shop.BLL.Interfaces;
+
+namespace Shop.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OrderController(IOrderService orderService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IEnumerable<OrderResponseDto>> GetOrdersAsync(CancellationToken token)
+    {
+        return await orderService.GetOrdersAsync(token);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<OrderResponseDto> GetOrderById(Guid id)
+    {
+        return await orderService.GetOrderByIdAsync(id);
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<OrderResponseDto>> GetOrdersByUserId([FromBody] Guid userId)
+    {
+        return await orderService.GetOrdersByUserIdAsync(userId);
+    }
+
+    [HttpPost]
+    public async Task Post([FromBody] OrderRequestCreationDto orderRequest)
+    {
+        await orderService.CreateOrderAsync(orderRequest);
+    }
+}
