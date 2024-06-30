f27cf1e730ef8f65ba4e48609521d06db3fafb3b
Kirill Meleshko <meleshkokirill004@gmail.com>
2024-06-30 21:44:16 +0300

    Created categries controller

--- Shop.BLL/Interfaces/ICategoryService.cs
+++ Shop.BLL/Interfaces/ICategoryService.cs
@@ -1,11 +1,15 @@
 using Shop.BLL.Common.DataTransferObjects.Categories;
 
-namespace Shop.BLL.Interfaces{
-    public interface ICategoryService{ 
-        Task<IEnumerable<CategoryResponseDto>> GetCategoriesAsync(); 
-        Task<CategoryResponseDto> GetCategoryByIdAsync(Guid id); 
-        Task CreateCategoryAsync(CategoryRequestCreationDto categoryRequestCreationDto);
-        Task UpdateCategoryAsync(Guid id, CategoryRequestUpdateDto categoryRequestUpdateDto); 
-        Task DeleteCategoryAsync(Guid id); 
+namespace Shop.BLL.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryResponseDto>> GetCategoriesAsync(CancellationToken cancellationToken);
+        Task<CategoryResponseDto> GetCategoryByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<CategoryResponseDto> CreateCategoryAsync(CategoryRequestCreationDto categoryRequestCreationDto,
+            CancellationToken cancellationToken);
+        Task UpdateCategoryAsync(Guid id, CategoryRequestUpdateDto categoryRequestUpdateDto,
+            CancellationToken cancellationToken);
+        Task DeleteCategoryAsync(Guid id, CancellationToken cancellationToken);
     }
 }
