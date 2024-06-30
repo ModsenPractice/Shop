f0c6ba8bf2c676d2d824b2a67f2c0926d1b18147
des1de <des1de377@gmail.com>
2024-06-30 17:22:53 +0300

    add user controller

---
+++ Shop.API/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shop.BLL.Common.DataTransferObjects.Users;
+using Shop.BLL.Interfaces;
+
+namespace Shop.API.Controllers; 
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IUserService userService) : ControllerBase
+{
+    [HttpGet]
+    [Authorize(Policy = "Admin")]
+    public async Task<IActionResult> GetUsersAsync()
+    {
+        var users = await userService.GetUsersAsync();
+        return Ok(users); 
+    }
+
+    [HttpGet("{id:guid}")]
+    [Authorize(Policy = "Admin")]
+    public async Task<IActionResult> GetUserByIdAsync(Guid id)
+    {
+        var user = await userService.GetUserByIdAsync(id);
+        return Ok(user);  
+    }
+
+    [HttpPut("{id:guid}")]
+    [Authorize(Policy = "Admin")]
+    public async Task<IActionResult> Put(Guid id,[FromBody] UserRequestUpdateDto userRequestDto)
+    {
+        await userService.UpdateUserAsync(id, userRequestDto);
+        return NoContent(); 
+    }
+
+    [HttpDelete("{id:guid}")]
+    [Authorize(Policy = "Admin")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await userService.DeleteUserAsync(id);
+        return NoContent(); 
+    }
+}
